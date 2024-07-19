517313a6c08871b6f0df2f3070806a21c12f73a9
Aftherhourspianist <65253360+sinpack@users.noreply.github.com>
2024-07-19 22:32:35 +0300

    ./ in next config

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     loader: 'akamai',
-    path: '/', // <----- THIS IS THE ISSUE
+    path: './', // <----- THIS IS THE ISSUE
   },
 };
 
