ca082c9867456e4624b407b570a5ffd57796e2f1
Aftherhourspianist <65253360+sinpack@users.noreply.github.com>
2024-07-19 22:16:31 +0300

    static exports

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: '/joe',
-  // output: 'export', // <=== enables static exports
-  // reactStrictMode: true,
+  basePath: '/joe',
+  output: 'export', // <=== enables static exports
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
