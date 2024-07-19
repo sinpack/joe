2928133d99c6fa391f91b3622717df5207d21a4e
Aftherhourspianist <65253360+sinpack@users.noreply.github.com>
2024-07-19 22:29:58 +0300

    path / in images nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   basePath: '/joe',
   output: 'export', // <=== enables static exports
   reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '/', // <----- THIS IS THE ISSUE
+  },
 };
 
 export default nextConfig;
