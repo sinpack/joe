93f32ae4c332cb18ec815d9639120428eaf281a8
Aftherhourspianist <65253360+sinpack@users.noreply.github.com>
2024-07-19 22:45:30 +0300

    classic path

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/joe',
   output: 'export', // enables static exports
   reactStrictMode: true,
   images: {
     loader: 'default', // Use default loader or 'imgix', 'cloudinary', etc.
-    path: '/joe/', // Adjust path based on basePath and where your images are located
+    path: '/', // Adjust path based on basePath and where your images are located
   },
 };
 
