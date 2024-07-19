b9cd467af933e57e33572d0885aa4e556b997429
Aftherhourspianist <65253360+sinpack@users.noreply.github.com>
2024-07-19 22:20:24 +0300

    next config remove images key

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,6 @@ const nextConfig = {
   basePath: '/joe',
   output: 'export', // <=== enables static exports
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
 };
 
 export default nextConfig;
