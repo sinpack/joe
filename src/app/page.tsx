b10e27a4f0104c34133be4cf230135993c28d99d
Aftherhourspianist <65253360+sinpack@users.noreply.github.com>
2024-08-10 01:10:53 +0300

    Home page refinements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import sliderData from '../utils/sliderData';
 import Image from 'next/image';
 import holistic from '../../public/holistic2.jpg';
 import NavigationButton from './components/Buttons/NavigateButton';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
   const OPTIONS: EmblaOptionsType = {
@@ -14,14 +15,28 @@ export default function Home() {
     watchResize: true,
   };
 
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 480);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  console.log(isMobile);
   return (
     <main className="flex min-h-screen flex-col items-center justify-center w-full bg-sky-50">
       <HeroSection />
-      <section className="container mx-auto py-40 px-4 bg-sky-50">
-        <div className="flex flex-col justify-center text-center px-20 lg:px-60">
-          <h2 className="mt-4">
+      <section className="container mx-auto py-40 bg-sky-50 ">
+        <div className="flex flex-col justify-center text-center px-2.5 md:px-40 lg:px-60">
+          <h2 className="mt-5">
             Ονομάζομαι Γεώργιος Αντωνόπουλος και είμαι πιστοποιημένος Ολιστικός
             Coach, διαθέτοντας πλούσιο υπόβαθρο στις Τέχνες και τις Επιστήμες.
+            <br className="lg:hidden" />
             <a href="/bio" className="text-blue-600 text-base ml-2.5">
               Διαβάστε περισσότερα
             </a>
@@ -31,7 +46,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section className="relative w-full h-[40vh] overflow-hidden">
+      <section className="relative w-full h-[80vh] overflow-hidden">
         {/* Background image */}
         <div className="absolute inset-0">
           <Image
@@ -45,43 +60,47 @@ export default function Home() {
             priority
           />
         </div>
-
-        <div className="flex flex-col items-center justify-between text-center container mx-auto px-20 lg:px-64 space-y-5 relative z-10 h-full">
-          <div className="flex flex-col space-y-5">
-            <h2 className="text-3xl font-bold text-center text-grey-600 mt-5">
+        <div className="flex flex-col items-center justify-between text-center container mx-auto  space-y-5 relative z-10 h-full">
+          <div className="flex flex-col space-y-5 px-2.5 md:px-40 lg:px-60 lg:mt-10">
+            <h1 className="font-bold text-grey-600 mt-5 tracking-normal">
               Holistic Coaching{' '}
-            </h2>
-            <p className="mb-10 text-grey-600 font-bold">
+            </h1>
+            <h3 className="mb-10 text-grey-600 font-semibold">
               Το holistic coaching εστιάζει στην ισορροπία μεταξύ σώματος, νου,
               συναισθήματος και πνεύματος, προσφέροντας μια σφαιρική προσέγγιση
               για την αντιμετώπιση των προκλήσεων και την επίτευξη προσωπικών
               στόχων.
+              <br className="lg:hidden" />
               <a
                 href="/holistic-coaching"
                 className="text-blue-600 text-base ml-2.5"
               >
                 Διαβάστε περισσότερα
               </a>
-            </p>
+            </h3>
           </div>
         </div>
       </section>
       <section className="container mx-auto py-40 px-4">
-        <div className="flex flex-col justify-center text-center px-20 lg:px-60 space-y-5">
-          <h2 className="text-3xl font-bold underline">
+        <div className="flex flex-col justify-center text-center px-2.5 md:px-40 lg:px-60 space-y-5">
+          <h2 className="text-2xl md:text-3xl font-bold underline">
             Υπηρεσίες - Θεραπείες{' '}
           </h2>
-          <p className="mb-10">
+          <h3 className="mb-10">
             Ως holistic - integrative coach, προσφέρω μια σειρά υπηρεσιών
             προσαρμοσμένων στις μοναδικές σας ανάγκες και στόχους. Μαζί,
             μπορούμε να εργαστούμε για την ευημερία σας.
             <a href="/treatments" className="text-blue-600 text-base ml-2.5">
               Διαβάστε περισσότερα
             </a>
-          </p>
+          </h3>
           <EmblaCarousel slides={sliderData} options={OPTIONS} />
           <div className="flex place-self-center pt-10">
-            <NavigationButton text="ΚΛΕΙΣΤΕ ΡΑΝΤΕΒΟΥ" link="/contact" />
+            <NavigationButton
+              text="ΚΛΕΙΣΤΕ ΡΑΝΤΕΒΟΥ"
+              link="/contact"
+              width={isMobile ? 200 : undefined}
+            />
           </div>
         </div>
       </section>
