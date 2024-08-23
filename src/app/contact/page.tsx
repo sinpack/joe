e5a3dbc088ae5f0e5f042c5737899dabfd2c82d9
Aftherhourspianist <65253360+sinpack@users.noreply.github.com>
2024-08-24 02:04:19 +0300

    contact page paddings margins fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,8 +6,7 @@ import ScrollToTopButton from '../components/Buttons/ScrollToTopButton';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title:
-    'Επικοινωνία Γιώργου Αντωνόπουλου',
+  title: 'Επικοινωνία Γιώργου Αντωνόπουλου',
 };
 
 export default function Contact() {
@@ -16,8 +15,8 @@ export default function Contact() {
   });
 
   return (
-    <section className="bg-sky-50 py-20 px-2.5 container mx-auto">
-      <div className="flex flex-col items-center">
+    <section className="bg-sky-50 py-20">
+      <div className="flex flex-col items-center px-2.5 container mx-auto">
         <main className="flex flex-col w-full max-w-3xl">
           <div className="flex flex-col items-center py-10 bg-sky-50 space-y-20">
             <h1 className="flex w-fit justify-center items-center text-4xl tracking-widest font-bold whitespace-normal">
@@ -32,28 +31,26 @@ export default function Contact() {
         </main>
       </div>
       <BlueDivider polygon={false} />
-        <div className="items-center w-full py-20">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-            {contactDetails.map((detail, index) => (
-              <div
-                key={index}
-                className="flex flex-col items-center lg:items-center text-left text-wrap"
-              >
-                <div className="mb-4">{detail.icon}</div>
-                <span className="font-semibold text-lg mb-2">
-                  {detail.title}
-                </span>
-                {detail.description && (
-                  <p className="text-gray-600 text-center w-full break-words whitespace-normal">
-                    {detail.description}
-                  </p>
-                )}
-                {detail.icons && detail.icons}
-              </div>
-            ))}
-          </div>
+      <div className="items-center w-full py-20 px-2.5 container mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          {contactDetails.map((detail, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center lg:items-center text-left text-wrap"
+            >
+              <div className="mb-4">{detail.icon}</div>
+              <span className="font-semibold text-lg mb-2">{detail.title}</span>
+              {detail.description && (
+                <p className="text-gray-600 text-center w-full break-words whitespace-normal">
+                  {detail.description}
+                </p>
+              )}
+              {detail.icons && detail.icons}
+            </div>
+          ))}
         </div>
-      <div className="flex flex-col w-full z-10 container mx-auto py-20 px-2.5">
+      </div>
+      <div className="flex flex-col w-full z-10 py-20 px-2.5 container mx-auto">
         <DynamicMap />
       </div>
       <ScrollToTopButton />
