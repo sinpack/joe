75ba6b10ddd993424d95dcd857451b674ec9527c
Aftherhourspianist <65253360+sinpack@users.noreply.github.com>
2024-09-06 18:22:32 +0300

    categories list section in treatments, text adjustments

---
+++ src/app/treatments/CategoryList.tsx
@@ -0,0 +1,57 @@
+// components/CategoryList.tsx
+
+import React from 'react';
+import Divider from '../components/Divider';
+import Image from 'next/image';
+import bulletImage4 from '../../../public/bullet4.png';
+
+const categories = [
+  'Μείωση του στρες',
+  'Διακοπή καπνίσματος (1 συνεδρία)',
+  'Απώλεια βάρους',
+  'Επίτευξη προσωπικών / επαγγελματικών στόχων',
+  'Θέματα σχέσεων (επαγγελματικές, διαπροσωπικές, ερωτικές, κτλ.)',
+  'Αύξηση αυτοπεποίθησης – αυτοεκτίμησης',
+  'Αλλαγή επαγγελματικής καριέρας',
+  'Αυτοβελτίωση – προσωπική εξέλιξη',
+  'Περιοριστικές πεποιθήσεις',
+  'Συναισθηματική διαχείριση',
+  'Αντιμετώπιση προβλημάτων',
+  'Εύρεση εσωτερικού σκοπού',
+];
+
+const CategoryList: React.FC = () => {
+  return (
+    <div className="flex flex-col w-full space-y-5">
+      <div className="flex flex-row items-center justify-center space-x-5">
+        <Divider className="border-gray-400 border" />
+        <h1 className="text-center py-5 text-nowrap">ΚΑΤΗΓΟΡΙΕΣ</h1>
+        <Divider className="border-gray-400 border" />
+      </div>{' '}
+      <ul className="flex flex-col self-center rounded-xl border border-gray-300 border-b-0 shadow-xl shadow-zinc-700 drop-shadow-xl">
+        {categories.map((category, index) => (
+          <li
+            key={index}
+            className="flex px-2.5 py-4 min-w-fit bg-sky-100 first:rounded-t-xl border-b last:rounded-b-xl border-b-gray-300 items-center"
+          >
+            {}
+            <div className="flex-shrink-0">
+              <Image
+                src={bulletImage4}
+                alt="bullet point"
+                width={18}
+                height={18}
+                className="w-4 h-4"
+              />
+            </div>
+            <p className="ml-2 leading-tight tracking-tight font-medium text-sm md:text-lg">
+              {category}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CategoryList;
--- src/app/treatments/page.tsx
+++ src/app/treatments/page.tsx
@@ -7,17 +7,17 @@ import clsx from 'clsx';
 import ScrollToTopButton from '../components/Buttons/ScrollToTopButton';
 import { Metadata } from 'next';
 import NavigationButton from '../components/Buttons/NavigationButton';
+import CategoryList from './CategoryList';
 
 export const metadata: Metadata = {
-  title:
-    'Υπηρεσίες-Θεραπείες Γιώργου Αντωνόπουλου',
+  title: 'Υπηρεσίες-Θεραπείες Γιώργου Αντωνόπουλου',
 };
 
 const Treatments = () => {
   return (
     <>
-      <section>
-        <div className="flex flex-col items-center justify-start bg-sky-50 py-20 px-20 lg:px-60 md:px-40 sm:px-30 w-full">
+      <section className="container mx-auto px-5 lg:px-20 py-10 bg-sky-50 mb-5">
+        <div className="flex flex-col items-center justify-start space-y-5">
           <div className="flex flex-col items-center gap-5 h-auto max-w-xl w-full">
             <div className="flex flex-col justify-start w-full">
               <p className="text-center whitespace-normal">
@@ -26,6 +26,7 @@ const Treatments = () => {
                 μπορούμε να εργαστούμε για την ευημερία σας.
               </p>
             </div>
+            <CategoryList />
           </div>
         </div>
       </section>
@@ -34,7 +35,9 @@ const Treatments = () => {
       <section className="flex flex-col container mx-auto px-5 lg:px-20 py-10 bg-sky-50">
         <div className="flex flex-row items-center justify-center space-x-5">
           <Divider className="border-gray-400 border" />
-          <h1 className="text-center py-10 text-nowrap">ΥΠΗΡΕΣΙΕΣ-ΘΕΡΑΠΕΙΕΣ</h1>
+          <h1 className="text-center py-10 text-nowrap">
+            ΥΠΗΡΕΣΙΕΣ - ΘΕΡΑΠΕΙΕΣ
+          </h1>
           <Divider className="border-gray-400 border" />
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-6">
@@ -43,11 +46,9 @@ const Treatments = () => {
               <div
                 key={index}
                 className={clsx({
-                  "lg:col-span-2":true,
-                  'lg:col-span-1 lg:col-start-2':
-                    index === 6,
-                    'lg:col-span-1 lg:col-start-4':
-                    index === 7,
+                  'lg:col-span-2': true,
+                  'lg:col-span-1 lg:col-start-2': index === 6,
+                  'lg:col-span-1 lg:col-start-4': index === 7,
                   ' lg:first:border-gray-400 lg:first:border-r-2 lg:first:border-b-2 lg:[&:nth-child(2)]:border-gray-400 lg:[&:nth-child(2)]:border-b-2 lg:[&:nth-child(2)]:border-r-2 lg:[&:nth-child(3)]:border-gray-400 lg:[&:nth-child(3)]:border-b-2 lg:[&:nth-child(3)]:border-r-0 lg:[&:nth-child(4)]:border-gray-400 lg:[&:nth-child(4)]:border-b-2 lg:[&:nth-child(4)]:border-r-2 lg:[&:nth-child(5)]:border-gray-400 lg:[&:nth-child(5)]:border-b-2 lg:[&:nth-child(5)]:border-r-2 lg:[&:nth-child(6)]:border-gray-400 lg:[&:nth-child(6)]:border-b-2 lg:[&:nth-child(7)]:border-gray-400 lg:[&:nth-child(7)]:border-r-2':
                     true,
                   'sm:first:border-gray-400 sm:first:border-r-2 sm:first:border-b-2 sm:[&:nth-child(2)]:border-gray-400 sm:[&:nth-child(2)]:border-b-2 sm:[&:nth-child(3)]:border-gray-400 sm:[&:nth-child(3)]:border-b-2 sm:[&:nth-child(3)]:border-r-2 sm:[&:nth-child(4)]:border-gray-400 sm:[&:nth-child(4)]:border-b-2 sm:[&:nth-child(5)]:border-gray-400 sm:[&:nth-child(5)]:border-b-2 sm:[&:nth-child(5)]:border-r-2 sm:[&:nth-child(6)]:border-gray-400 sm:[&:nth-child(6)]:border-b-2 sm:[&:nth-child(7)]:border-gray-400 sm:[&:nth-child(7)]:border-r-2':
@@ -60,7 +61,7 @@ const Treatments = () => {
                   descriptions={treatment.descriptions}
                   link={`/treatments/${treatment.nameId}`}
                   index={index}
-                  descriptionCardClassName= "h-full"
+                  descriptionCardClassName="h-full"
                 />
               </div>
             );
