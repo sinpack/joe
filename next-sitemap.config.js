624f5147c61c36550a6091f48c0226c98ebef025
Aftherhourspianist <65253360+sinpack@users.noreply.github.com>
2024-09-18 15:03:39 +0300

    sitemap siteurl fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,9 @@
 const dev = process.env.NODE_ENV !== 'production';
 
 const config = {
-  siteUrl: dev ? 'http://localhost:3000' : 'https://your-deployed-site-url.com',
+  siteUrl: dev
+    ? 'http://localhost:3000'
+    : 'https://www.georgiosantonopoulos.gr',
   generateRobotsTxt: true, // Optional: Generate a robots.txt file
   // changefreq: 'daily', // Optional: Frequency of change
   // priority: 0.7, // Optional: Default priority
