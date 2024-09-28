74c93d209c42bad518b4d4318a18bfb7f5e4e1dd
Aftherhourspianist <65253360+sinpack@users.noreply.github.com>
2024-09-29 01:12:19 +0300

    head preload in hero section

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 import Image from 'next/image';
 import introPhoto from '../../../public/intro5.webp';
 import { handwritingFont } from './../layout';
+import Head from 'next/head';
 
 const Hero: React.FC = () => {
   return (
     <>
+      {/* Preload the image in the <head> section */}
+      <Head>
+        <link
+          rel="preload"
+          as="image"
+          href={introPhoto.src} // The src of the image for preloading
+          type="image/webp"
+        />
+      </Head>
+
       <section className="relative w-full h-[70vh] overflow-hidden">
         <div className="absolute inset-0">
           <Image
@@ -16,13 +27,10 @@ const Hero: React.FC = () => {
             quality={100}
             placeholder="blur"
             priority
-            // sizes="(max-width: 768px) 100vw,
-            //      (max-width: 1024px) 50vw,
-            //      33vw" // Responsive image sizes
             fetchPriority="high"
           />
         </div>
-        <div className="relative z-10 flex items-center h-full justify-center lg:justify-start ml-0 lg:ml-20 container mx-auto ">
+        <div className="relative z-10 flex items-center h-full justify-center lg:justify-start ml-0 lg:ml-20 container mx-auto">
           <h1
             className={`${handwritingFont.className} !italic text-gray-500 text-xl md:text-2xl lg:text-3xl text-center tracking-tight`}
           >
